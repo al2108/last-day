d637aecb0776049cf835f47703021f5bb434652c
al2108 <alewark@gmail.com>
2019-10-06 12:21:58 +0200

    2 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,19 @@
 import { Component } from '@angular/core';
-import * as moment from 'moment';
+const moment = require('moment');
+
+const holidays = [
+  moment('2023-01-01'),
+  moment('2023-01-06'),
+  moment('2023-04-07'),
+  moment('2023-04-10'),
+  moment('2023-05-18'),
+  moment('2023-05-29'),
+  moment('2023-06-08'),
+  moment('2023-10-03'),
+  moment('2023-11-01'),
+  moment('2023-12-25'),
+  moment('2023-12-26'),
+];
 
 @Component({
   selector: 'my-app',
@@ -8,14 +22,30 @@ import * as moment from 'moment';
 })
 export class AppComponent  {
   public hoursLongTermInitial = 500;
+  public daysVacation = 30 / 12 * 9;
   public lastDayCalculated: string;
+
 
   constructor() {
     let lastDay;
-    let hoursLongTerm = this.hoursLongTermInitial;
+    let isHoliday: boolean;
+    let hoursFree = this.hoursLongTermInitial + this.daysVacation * 7;
     lastDay = moment('2023.09.30');
-    while (hoursLongTerm > 0) {
-      hoursLongTerm -= 7;
+    while (hoursFree > 0) {
+      if ((lastDay.day() !== 0) && (lastDay.day() !== 6)) {
+        // this is not a sunday or saturday
+        isHoliday = false;
+        for (let holiday of holidays) {
+          if (lastDay.isSame(holiday, 'day')) {
+            // this is a holiday
+            isHoliday = true;
+            console.log('found holiday: ', holiday);
+          }
+        }
+        if (!isHoliday) {
+          hoursFree -= 7;
+        }
+      }
       lastDay.add(-1, 'days');
     }
     this.lastDayCalculated = lastDay.format('DD.MM.YYYY');
