4033059e05f470b476bfc762b724fe756b98fdeb
al2108 <42086868+al2108@users.noreply.github.com>
2022-11-08 12:58:04 +0100

    1 file modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -72,7 +72,7 @@ export class AppComponent {
   public onReset() {
     this.hoursShortTermInitial = 0;
     this.hoursLongTermInitial = 530;
-    this.daysVacation2023 = (30 / 12) * 9;
+    this.daysVacation2023 = 23;
     this.daysVacationBefore2023 = 8;
     this.recalculate();
   }
